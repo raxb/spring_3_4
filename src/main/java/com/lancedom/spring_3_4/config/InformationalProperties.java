99f01b61fd635757f7f0a6551e16703f67d8e36c
Vivek Ravikumar <vravikumar@fispan.com>
2025-01-04 19:34:14 -0800

    updating with lombok

--- src/main/java/com/lancedom/spring_3_4/config/InformationalProperties.java
+++ src/main/java/com/lancedom/spring_3_4/config/InformationalProperties.java
@@ -3,6 +3,9 @@ package com.lancedom.spring_3_4.config;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +13,9 @@ import org.springframework.validation.annotation.Validated;
 @Configuration
 @ConfigurationProperties(prefix = "info")
 @Validated
+@Getter
+@Setter
+@ToString
 public class InformationalProperties {
 
     @NotNull
@@ -18,35 +24,8 @@ public class InformationalProperties {
     @Email
     private String email;
 
-    @Valid //the validation to cascade down to nested properties - https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.4-Release-Notes#bean-validation-of-configuration-properties
+    @Valid
+    //the validation to cascade down to nested properties - https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.4-Release-Notes#bean-validation-of-configuration-properties
     private Address address;
 
-    public @NotNull String getName() {
-        return name;
-    }
-
-    public void setName(@NotNull String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "InformationalProperties{" + "name='" + name + '\'' + ", email='" + email + '\'' + ", address=" + address + '}';
-    }
-
-    public @Email String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@Email String email) {
-        this.email = email;
-    }
-
-    public @Valid Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(@Valid Address address) {
-        this.address = address;
-    }
 }
